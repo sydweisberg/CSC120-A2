00e3d46cf2f32b4625d48a4c21cca8b068df0e06
sydweisberg <113071546+sydweisberg@users.noreply.github.com>
2022-09-19 20:05:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 from calendar import c
 from typing import Dict, Union
 
-# Import the functions we wrote in procedural_resale_shop.py
-#from procedural_resale_shop import buy, update_price, sell, print_inventory, refurbish
+# Imports everything from the computer.py and oo_resale_shop.py files
 from computer import *
 from oo_resale_shop import *
 
 def main():
-    resaleshop = ResaleShop([],0)
-    computer = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500)
+    resaleshop = ResaleShop([],0) # Initializes the resaleshop
+    computer = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500) # Creates a computer object
     print("-" * 21)
     print("COMPUTER RESALE STORE")
     print("-" * 21)
